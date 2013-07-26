e57c7195ee288d8daf23368eb1d7e6c718195b72
Jenney Grover <jenneygrover@fullsail.edu>
2013-07-25 22:56:20 -0400

    validator
    starting validator code

--- gold/js/main.js
+++ gold/js/main.js
@@ -4,47 +4,47 @@
 	July 25th, 2013
 */
 
-$(document).ready(function(){
-	$('#home').on('pageinit', function(){
-		//code needed for home page goes here
-	});	
-		
-	$('#addItem').on('pageinit', function(){
+$('#home').on('pageinit', function(){
+	//code needed for home page goes here
+});	
+	
+$('#addItem').on('pageinit', function(){
 
-			var myForm = $('#formId');
-				myForm.validate({
-				invalidHandler: function(form, validator) {
-				},
-				submitHandler: function() {
-			var data = myForm.serializeArray();
+		var addBook = $('#formId');
+			addBook.validate({
+			invalidHandler: function(form, validator) {
+				
+			},
+			submitHandler: function() {
+				var data = addBook.serializeArray();
 				storeData(data);
 			}
-		});
-	
-		//any other code needed for addItem page goes here
-	
-	});
+});
 
-	//The functions below can go inside or outside the pageinit function for the page in which it is needed.
-
-	var autofillData = function (){
-	 
-	};
-
-	var getData = function(){
-
-	};
-
-	var storeData = function(data){
-	
-	}; 
-
-	var	deleteItem = function (){
-			
-	};
-					
-	var clearLocal = function(){
-
-	};
+	//any other code needed for addItem page goes here
 
 });
+
+//The functions below can go inside or outside the pageinit function for the page in which it is needed.
+
+var autofillData = function (){
+ 
+};
+
+var getData = function(){
+
+};
+
+var storeData = function(data){
+
+}; 
+
+var	deleteItem = function (){
+		
+};
+				
+var clearLocal = function(){
+
+	};
+
+
